346f8676e653bf2b0ddc73b16df666fb89296527
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2025-12-15 09:29:44 +0200

    initial skeleton for the app

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,35 @@
-import Header from "./_components/Header";
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
-import "@/app/_styles/globals.css";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
 
 import { Josefin_Sans } from "next/font/google";
-const josefinSans = Josefin_Sans({
-  subsets: ["latin"], //set of chars
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
   display: "swap",
 });
 
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
 export const metadata = {
-  // title: "The Wild Oasis",
   title: {
-    default: "Welcome to The Wild Oasis",
     template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
   },
   description:
-    "Your paradise in the Dolomites. Comfortable cabins, stunning mountain views, and a warm welcome to nature's beauty.",
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased ${josefinSans.className}`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
